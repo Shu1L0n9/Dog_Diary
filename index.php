353fabdad3d574bd2f4cf416f581c28c82baf44e
Mysaku Einzbern <einzmysaku@gmail.com>
2022-06-23 08:51:25 +0800

    inital commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>舔狗日记</title>
+		<meta name="keywords" content="">
+        <meta name="description" content="">
+        <link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="static/css/main.css" />
+		<noscript><link rel="stylesheet" href="static/css/noscript.css" /></noscript>
+	</head>
+	<body onselectstart="return false" oncontextmenu=self.event.returnValue=false> 
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span>舔狗日记</span>						</div>
+						<div class="content">
+							<div class="inner">
+								<h2><?php echo date("Y年m月d日D")?>   晴</h2>
+								<h3><script src="./api.php "></script></h3>
+							</div>
+						</div>
+					</header>
+
+					<footer id="footer">
+					</footer>
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="static/js/jquery.min.js"></script>
+			<script src="static/js/skel.min.js"></script>
+			<script src="static/js/util.js"></script>
+			<script src="static/js/main.js"></script>
+ 
+
+	</body>
+</html>
